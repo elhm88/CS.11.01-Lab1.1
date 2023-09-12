f93e96380bdc4b3a1465b710beaeb03c70d21400
elhm88 <elhm88@student.cis.edu.hk>
2023-09-12 13:53:22 +0800

    Completed Lab 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        int age;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+
+        age = scanner.nextInt();
+
+        System.out.print(age + "-years olds should date somebody who is at least " + ((age/2)+7) + " years old.");
+
+        System.out.print("\n\nCredit: Thanks to Jim Cohoon at The University of Virginia");
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class Pages {
 
     public static void main(String[] args) {
+        int age;
+
+        System.out.print("Enter your age: ");
+        Scanner scanner = new Scanner(System.in);
+
+        age = scanner.nextInt();
+
+        System.out.print(age + "-year olds should read at least " + (100-age) + " pages before giving up on a book.");
+
+        System.out.print("\n\nCredit: Thanks to Jim Cohoon at The University of Virginia");
 
     }
 }
